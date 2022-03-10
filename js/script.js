143bb4fdf426fe76ce8ee31c4c86afb342d01963
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-03-10 18:30:07 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Copyright (c) 2020 Johanna Liu All rights reserved
+// Copyright (c) 2022 Johanna Liu All rights reserved
 //
 // Created by: Johanna Liu
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  // this function does basic math
+
+document.getElementById('multiply-math').innerHTML = '<p>The area is: ' + (4 * 5 ) + '</p>'
+document.getElementById('add-math').innerHTML = '<p>The perimeter is: ' + (4 * 2 + 5 * 2 ) + '</p>'
+}
