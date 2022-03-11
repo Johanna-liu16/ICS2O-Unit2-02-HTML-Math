631c22d73159a16e2815f7dadde0df6a2935c48e
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-03-11 13:04:46 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -10,6 +10,6 @@
 function doMathClicked() {
   // this function does basic math
 
-document.getElementById('multiply-math').innerHTML = '<p>The area is: ' + (4 * 5 ) + '</p>'
-document.getElementById('add-math').innerHTML = '<p>The perimeter is: ' + (4 * 2 + 5 * 2 ) + '</p>'
+document.getElementById('multiply-math').innerHTML = '<p>The area is: ' + (4 * 5 ) + ' cm² </p>'
+document.getElementById('add-math').innerHTML = '<p>The perimeter is: ' + (4 * 2 + 5 * 2 ) +' cm </p>'
 }
